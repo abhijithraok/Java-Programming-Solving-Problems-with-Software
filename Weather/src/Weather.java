c770a551c6952b3d60c662341466050315e588d8
Abhijith Rao <abijithraok@gmail.com>
2017-05-31 01:10:05 +0530

    moved files from java repo

---
+++ Weather/src/Weather.java
@@ -0,0 +1,20 @@
+/**
+ * Created by abhij on 18-May-17.
+ */
+
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+public class Weather {
+    public static void main(String[] args) {
+        CSVMax a = new CSVMax();
+       //  a.testColdestHourInFile();
+        //  a.testFileWithColdestTemperature();
+        // a.testLowestHumidityInFile();
+        // a.testLowestHumidityInManyFiles();
+        //  a.testAverageTemperatureInFile();
+        //a.testAverageTemperatureWithHighHumidityInFile();
+       a.testFileWithColdestTemperature();
+
+    }
+}
---
+++ genetest/src/first.java
@@ -0,0 +1,10 @@
+/**
+ * Created by abhij on 13-May-17.
+ */
+public class first {
+    public  static void main(String [] args){
+        part3 a = new part3();
+        a.testProcessGenes();
+
+    }
+}
